d1aca87dd7c3f53eb10e0adf41af8d8a111fa585
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-25 14:28:47 -0300

    feat: create post /sign-up

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
 
 dotenv.config()
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
@@ -18,6 +19,24 @@ export default db;
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.post('/sign-up', async(req,res)=>{
+  const {name, email, password, confirmPassword} = req.body
+  try {
+    const user = await db.collection('users').findOne({email})
+
+    if(user) return res.sendStatus(409)
+    const SALT = 10
+    const hash = bcrypt.hashSync(password, SALT)
+    await db.collection('users').insertOne({name, email, password: hash})
+    return res.sendStatus(201)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+
+})
+
+
 
 const port = process.env.PORT || 5000;
 
