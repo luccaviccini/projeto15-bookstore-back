613ff6c2b5f9755ce7fd2d4bb01b4198b0d123e3
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-25 12:52:59 -0300

    feat: Connected to database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+try {
+  await mongoClient.connect()
+  db = mongoClient.db()
+  console.log("Connected to database")
+} catch (error) {
+  console.log("Error connecting to database.", error)
+}
+
+export default db;
 const app = express()
 app.use(express.json())
 app.use(cors())
