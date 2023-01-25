97a2e53b223daf317d0082e61263ccf4a4b47011
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-25 14:31:28 -0300

    feat: create signInSchema and signUpSchema

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
+import joi from "joi"
 
 dotenv.config()
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
@@ -19,6 +20,18 @@ export default db;
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+const signUpSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  confirmPassword: joi.ref('password')
+})
+
+const signInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required()
+})
 
 app.post('/sign-up', async(req,res)=>{
   const {name, email, password, confirmPassword} = req.body
